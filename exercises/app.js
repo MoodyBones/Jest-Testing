89c68e0a28e978d7efca56fe02e534321c94d490
Mel <jones.mel@gmail.com>
2020-04-28 11:57:05 +0200

    completed getLongest() & arg/type error testing

---
+++ exercises/app.js
@@ -0,0 +1,42 @@
+const STATUS = ['Slow starter', 'Getting there', 'Super user']
+
+const getNumberOfCompletedLessons = (arr) => arr.filter((e) => e === 1).length
+
+const getStatus = (arr) => {
+  const completedLessons = getNumberOfCompletedLessons(arr.slice(-7))
+  if (completedLessons > 4) {
+    return STATUS[2]
+  } else if (completedLessons > 2) {
+    return STATUS[1]
+  }
+  return STATUS[0]
+}
+
+const getLongest = (arr) => {
+  const longestStreakReducer = (positiveStreaks, currentDay) => {
+    if (currentDay) {
+      positiveStreaks[positiveStreaks.length - 1]++
+    } else {
+      positiveStreaks.push(0)
+    }
+    return positiveStreaks
+  }
+  return Math.max(...arr.reduce(longestStreakReducer, [0]))
+}
+
+const runChallenge = (arr) => {
+  if (!arr) {
+    return 'Sorry, wrong argument.'
+  } else if (Array.isArray(arr)) {
+    return [getNumberOfCompletedLessons(arr), getLongest(arr), getStatus(arr)]
+  } else {
+    return 'Sorry, wrong type.'
+  }
+}
+
+module.exports = {
+  runChallenge,
+  getNumberOfCompletedLessons,
+  getStatus,
+  getLongest,
+}
